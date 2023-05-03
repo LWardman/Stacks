27a923fd435e6bf1ad30b55c397566521cb41aa1
Lewis <lewis2304@hotmail.co.uk>
2023-05-03 12:53:27 +0100

    Added ValidParentheses function.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <deque>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+bool ValidParentheses(std::string s)
+{
+    std::deque<char> Stack;
+
+    for (char bracket : s)
+    {
+        if (bracket == '(' || bracket == '[' || bracket == '{')
+        {
+            Stack.push_back(bracket);
+        }
+
+        else if (bracket == ')')
+        {
+            if (Stack.empty() || Stack.back() != '(') return false;
+            Stack.pop_back();
+        }
+
+        else if (bracket == ']')
+        {
+            if (Stack.empty() || Stack.back() != '[') return false;
+            Stack.pop_back();
+        }
+
+        else if (bracket == '}')
+        {
+            if (Stack.empty() || Stack.back() != '{') return false;
+            Stack.pop_back();
+        }
+    }
+    return Stack.empty();
+}
+
+int main()
+{
     return 0;
 }
